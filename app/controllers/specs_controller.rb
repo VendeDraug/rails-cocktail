701c3b360e8f59a3f4ce9de6ec361c0d2543247f
morgan krischer <morgan.krischer@gmail.com>
2018-11-26 17:49:10 +0000

    CRUD 1 model done

--- app/controllers/specs_controller.rb
+++ app/controllers/specs_controller.rb
@@ -9,27 +9,37 @@ class SpecsController < ApplicationController
   end
 
   def new
-
+    @spec = Spec.new
   end
 
   def create
-
+    @spec = Spec.new(spec_params)
+    if @spec.save
+      redirect_to specs_path
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
   def update
-
+    @spec.update(spec_params)
+    redirect_to spec_path(@spec)
   end
 
   def destroy
     @spec.destroy
+    redirect_to specs_path
   end
 
   private
   def set_spec
     @spec = Spec.find(params[:id])
+  end
+
+  def spec_params
+    params.require(:spec).permit(:title, :description)
   end
 end
